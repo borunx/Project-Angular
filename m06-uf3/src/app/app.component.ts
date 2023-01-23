ff9c33a5095d8e3e672ad032867ecc6bfb22aea8
Jonatan Feliz <jonatanfelizf@gmail.com>
2023-01-23 23:25:32 +0100

    update

--- m06-uf3/src/app/app.component.ts
+++ m06-uf3/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'm06-uf3';
+
+  oculta_links!:boolean;
+
+  ngOnInit(): void {
+    this.oculta_links=true;
+  }
+  
 }
